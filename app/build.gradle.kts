50e28a718f83819f00a6a38e0c61f8b01030ca2a
Ebrahim Byagowi <ebrahim@gnu.org>
2023-05-14 11:43:56 +0330

    Use Kotlin dsl and a fixed keystore

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "io.github.persiancalendar.fixedclock"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "io.github.persiancalendar.fixedclock"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = rootProject.file("nightly.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+dependencies {
+}
